25d39d073b64bd5e8b668d6abb1823e17c637d39
Nevania <140127237+Nevania@users.noreply.github.com>
2023-11-11 23:34:19 +0100

    Agregar archivos de proyecto.

---
+++ EjercicioManejoDeExpreciones/Operaciones.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioManejoDeExpreciones
+{
+    internal class Operaciones
+    {
+        public int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Restar(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Dividir(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("No se puede dividir por cero");
+            }
+            return (double)a / b;
+        }
+    }
+
+}
+
---
+++ EjercicioManejoDeExpreciones/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using EjercicioManejoDeExpreciones;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Ingrese el primer número:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo número:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Operaciones op = new Operaciones();
+
+            Console.WriteLine("Suma: " + op.Sumar(a, b));
+            Console.WriteLine("Resta: " + op.Restar(a, b));
+            Console.WriteLine("Multiplicación: " + op.Multiplicar(a, b));
+            Console.WriteLine("División: " + op.Dividir(a, b));
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Por favor, ingrese solo números.");
+        }
+        catch (DivideByZeroException e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+    }
+}
